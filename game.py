d00f2645c8ca90fa298d00f254df3655c2a42310
Maik <g.gaston240898@gmail.com>
2024-03-22 20:01:01 -0300

    Trabajo finalizado

--- game.py
+++ game.py
@@ -4,25 +4,41 @@ import random
 words = ["python", "programación", "computadora", "código", "desarrollo",
          "inteligencia"]
 
-# El juego tiene un bug. Si no se inserta ninguna letra para adivinar, el valor de la
-# letra es un string vacío "". Para este caso, el juego marca como que es un acierto.
-# Modifica el mismo para que indique que es un error en este caso.
-
 # Elegir una palabra al azar
 secret_word = random.choice(words)
-# Número máximo de intentos permitidos
-max_attempts = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
+# Número máximo de intentos permitidos
+max_attempts = 10
 
 print("¡Bienvenido al juego de adivinanzas!")
-print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
+print("Seleccione un nivel de dificultad para jugar! Contamos con 3:")
+print("1. Fácil: En la palabra a adivinar se muestran todas las vocales por defecto.")
+print("2. Medio: Se muestra la primera y la última letra de la palabra.")
+print("3. Difícil: No se muestra ninguna letra de la palabra.")
+opc = input("Dificultad: ")
 
-word_displayed = "_" * len(secret_word)
-# Mostrarla palabra parcialmente adivinada
+while opc not in ["1", "2", "3"]:
+    print("Tipo de dificultad elegida incorrecta. Ingresa una correcta (1, 2, 3).")
+    opc = input("Dificultad: ")
+
+opc = int(opc)
+
+if opc == 1:
+    word_displayed = ''.join([letter if letter in "aeioóu" else "_" for letter in secret_word])
+    guessed_letters.extend("aeioóu")
+elif opc == 2:
+    word_displayed = secret_word[0] + "_" * (len(secret_word) - 2) + secret_word[-1]
+    guessed_letters.extend([secret_word[0], secret_word[-1]])
+elif opc == 3:
+    word_displayed = "_" * len(secret_word)
+
+print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 print(f"Palabra: {word_displayed}")
-failure_count =0
-while failure_count <3:
+
+failure_count = 0
+
+while failure_count < 3:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
 
@@ -30,29 +46,25 @@ while failure_count <3:
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
         continue
-        # Agregar la letra a la lista de letras adivinadas
 
+    # Agregar la letra a la lista de letras adivinadas
     guessed_letters.append(letter)
-    # Verificar si la letra está en la palabra secreta
 
-    if (letter == "") or (letter not in secret_word):
+    # Verificar si la letra está en la palabra secreta
+    if letter == "" or letter not in secret_word:
         print("Lo siento, la letra no está en la palabra.")
-        failure_count+=1
-    elif letter in secret_word:
+        failure_count += 1
+    else:
         print("¡Bien hecho! La letra está en la palabra.")
+
     # Mostrar la palabra parcialmente adivinada
-    letters = []
-    for letter in secret_word:
-        if letter in guessed_letters:
-            letters.append(letter)
-        else:
-            letters.append("_")
-    word_displayed = "".join(letters)
+    word_displayed = ''.join([letter if letter in guessed_letters else "_" for letter in secret_word])
     print(f"Palabra: {word_displayed}")
+
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
-        print(f"¡Felicidades! Has adivinado la palabra secreta:{secret_word}")
+        print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
-    print(f"¡Oh no! Te has equivado {failure_count} veces y no puedes seguir jugando.")
+    print(f"¡Oh no! Te has equivocado {failure_count} veces y no puedes seguir jugando.")
     print(f"La palabra secreta era: {secret_word}")
